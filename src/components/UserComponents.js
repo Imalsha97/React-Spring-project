f2bbecb8f4405384aa3c11af97a17da9ecabc4fe
Imalsha <imalsham092@gmail.com>
2021-09-06 22:26:37 -0700

    frontend

---
+++ src/components/UserComponents.js
@@ -0,0 +1,51 @@
+import React from "react";
+import UserService from "../service/UserService";
+
+class UserComponent extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            users : []
+        }
+    }
+    componentDidMount(){
+        UserService.getUsers().then((response) => {
+            this.setState({ users : response.data})
+        });
+    }
+
+    render (){
+        return(
+            <div className="container">
+                <h1 className = "text-center">User List</h1>
+                <table className = "table table-striped">
+                    <thead>
+                        <tr>
+                            <td>User Id</td>
+                            <td>User email</td>
+                            <td>User First name</td>
+                            <td>User Last name</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.users.map(
+                                user =>
+                                <tr key = {user.id}>
+                                    <td>{user.id}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.firstName}</td>
+                                    <td>{user.lastName}</td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+
+
+                </table>
+
+            </div>
+        )
+    }
+}
+export default UserComponent
